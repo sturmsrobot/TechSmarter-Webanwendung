81252dbcac2e5a532706e4ea4e254329d22b283b
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-04-08 14:16:36 +0200

    Verwendung der Datenbankverbindung und Modelle

---
+++ express-app/controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require("../models/User");
+
+// Route zum Abrufen aller Benutzer
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await Users.findAll();
+    res.json(users);
+  } catch (error) {
+    console.error("Fehler beim Abrufen des Benutzers:", error);
+    res.status(500).json({ message: "Interner Serverfehler!" });
+  }
+};
