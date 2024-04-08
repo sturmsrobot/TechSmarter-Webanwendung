07e04689e5e5ff48a42a0dc5f66ecef6c6b0f187
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-04-08 12:35:06 +0200

    datenbank_localhost

--- express-app/server.js
+++ express-app/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 const routes = require("./routes/routes");
 
 app.use("/api", routes); // Verwendung definierter Routen
