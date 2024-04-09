166ef9b14003a90a0501f7288863c3d6c445eb90
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-04-09 11:41:04 +0200

    create base sites + css

---
+++ react-app/src/Pages/HTML/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./HTML.module.css";
+
+const HTML = () => {
+  return (
+    <div className={styles.container}>
+      <h2>HTML</h2>
+      <p>
+        HTML steht für Hypertext Markup Language und wird verwendet, um die
+        Struktur von Webseiten zu definieren.
+      </p>
+    </div>
+  );
+};
+
+export default HTML;
