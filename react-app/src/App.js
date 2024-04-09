b4325cfa22c96470703601a817c23456116fe989
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-04-09 10:46:59 +0200

    add routes & import

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,24 @@
 import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Main from "./Pages/Main_Page";
+import Main from "./Pages/Main_Page/index";
+import HTML from "./Pages/HTML/index";
+import JavaScript from "./Pages/Javascript/index";
+import Login from "./Pages/Login/index";
+import Python from "./Pages/Python/index";
 
 function App() {
   return (
-    <div className="App">
-      <Main></Main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/HTML" element={<HTML />} />
+        <Route path="/Javascript" element={<JavaScript />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Python" element={<Python />} />
+        {/* <div className="App"><Main></Main></div> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
