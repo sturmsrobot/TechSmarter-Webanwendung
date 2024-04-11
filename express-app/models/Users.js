52687d90554c7dbf3a1b60304e31eb170fc44224
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-04-11 13:21:41 +0200

    Korrektur BackendRouten

--- express-app/models/Users.js
+++ express-app/models/Users.js
@@ -1,44 +1,40 @@
-const { DataTypes, Model } = require("sequelize");
+const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-class User extends Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        username: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-      },
-      {
-        sequelize, // Übergeben Sie das Sequelize-Objekt hier
-        modelName: "User",
-      }
-    );
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // sequelize, // Übergeben Sie das Sequelize-Objekt hier
+    // modelName: "User",
   }
-}
+);
 
-
-// Definieren der Assoziationen, falls erforderlich
+//Definieren der Assoziationen, falls erforderlich
 User.associate = (models) => {
-  // Definition der Assoziationen, falls benötigt
+  //Definition der Assoziationen, falls benötigt
 };
 
-// Statische Methode für Benutzersuche nach Benutzernamen
+//Statische Methode für Benutzersuche nach Benutzernamen
 User.findByUsername = async function (username) {
   try {
     const users = await User.findAll({ where: { username } });
