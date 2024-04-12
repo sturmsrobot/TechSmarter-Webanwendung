49409402d3bf7d9efd295343924d26ea8c8e595f
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-04-12 08:57:38 +0200

    API_UsersRouten_Frontend

--- react-app/src/api/users/UserMutations.js
+++ react-app/src/api/users/UserMutations.js
@@ -1,50 +1,42 @@
 import api from "../../config/api";
 
-async function createNewUser(newUserId, newUserName, newUserEmail, newUserPassword) {
-  const result = await api.post("/user/create", {
-    newUserId: newUserId,
-   newUserName:newUserName,
-   newUserEmail:newUserEmail,
-   newUserPassword:newUserPassword
-  });
-
-  const newUser = result.data.id;
-
-  return newUser;
+export async function createNewUser(
+  newUserName,
+  newUserEmail,
+  newUserPassword
+) {
+  try {
+    const result = await api.post("/api/users", {
+      username: newUserName,
+      email: newUserEmail,
+      password: newUserPassword,
+    });
+    const newUser = result.data;
+    return newUser;
+  } catch (error) {
+    console.error("Fehler beim Erstellen eines neuen Benutzers:", error);
+    throw error;
+  }
 }
 
-async function markTodo(todoId, isDone) {
-  const result = await api.put("/todos/mark", {
-    todoId: todoId,
-    newIsDone: isDone,
-  });
-
-  const todo = result.data.updatedTodo;
-
-  return todo;
+export async function updateUser(userId, updatedUserData) {
+  try {
+    const result = await api.put(`/api/users/${userId}`, updatedUserData);
+    const updatedUser = result.data;
+    return updatedUser;
+  } catch (error) {
+    console.error("Fehler beim Aktualisieren des Benutzers:", error);
+    throw error;
+  }
 }
 
-async function updateTodo(updateId, updateTask, updateIsDone, updateDueDate) {
-  const result = await api.put("/todos/update", {
-    todoId: updateId,
-    newTask: updateTask,
-    newIsDone: updateIsDone,
-    newDueDate: updateDueDate,
-  });
-
-  const todo = result.data.updatedTodo;
-
-  return todo;
+export async function deleteUser(userId) {
+  try {
+    const result = await api.delete(`/api/users/${userId}`);
+    const deletedUserId = result.data;
+    return deletedUserId;
+  } catch (error) {
+    console.error("Fehler beim Löschen des Benutzers:", error);
+    throw error;
+  }
 }
-
-async function deleteTodo(todoId) {
-  const result = await api.delete("/todos/delete", {
-    data: { todoId },
-  });
-
-  const deletedTodoId = result.data.deletedTodosId;
-
-  return deletedTodoId;
-}
-
-export default { createNewUser, }
