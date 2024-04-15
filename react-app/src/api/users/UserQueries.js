9bdb1d67b88a460f80c02aa1d388901c3787dbdc
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-04-15 10:38:07 +0200

    Fehlerbehebung in den Routen

--- react-app/src/api/users/UserQueries.js
+++ react-app/src/api/users/UserQueries.js
@@ -1,23 +1,57 @@
 import api from "../../config/api";
 
+//Benutzer abrufen
 export async function fetchAllUsers() {
   try {
-    const result = await api.get("/api/users/all");
+    const result = await api.get("/api/users");
     const users = result.data;
     return users;
   } catch (error) {
-    console.error("Fehler beim Abrufen aller Benutzer:", error);
-    throw error;
+    throw new Error("Fehler beim Abrufen aller Benutzer: " + error.message);
   }
 }
 
+// Statistiken abrufen
+export async function fetchAllStats() {
+  try {
+    const result = await api.get("/api/stats");
+    const stats = result.data;
+    return stats;
+  } catch (error) {
+    throw new Error("Fehler beim Abrufen der Statistiken: " + error.message);
+  }
+}
+
+// Abfrage von Quizfragen
+export async function fetchAllQuestions() {
+  try {
+    const result = await api.get("/api/question");
+    const questions = result.data;
+    return questions;
+  } catch (error) {
+    throw new Error("Fehler beim Abrufen der Fragen: " + error.message);
+  }
+}
+
+//Abfrage von Antworten
+export async function fetchAllAnswers() {
+  try {
+    const result = await api.get("/api/answer");
+    const answers = result.data;
+    return answers;
+  } catch (error) {
+    throw new Error("Fehler beim Abrufen der Antworten: " + error.message);
+  }
+}
+
+// Benutzer nach ID abrufen
 export async function fetchUserById(userId) {
   try {
     const result = await api.get(`/api/users/${userId}`);
     const user = result.data;
     return user;
   } catch (error) {
-    console.error("Fehler beim Abrufen des Benutzers nach ID:", error);
+    console.error("Fehler beim Abrufen des Benutzers nach ID:", error.message);
     throw error;
   }
 }
