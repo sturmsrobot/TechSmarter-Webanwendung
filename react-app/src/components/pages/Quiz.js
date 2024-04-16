79b860cd804ea8805fcd9c33e86051bd80c8c842
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-04-16 20:14:29 +0200

    add mui spinner

--- react-app/src/components/pages/Quiz.js
+++ react-app/src/components/pages/Quiz.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import questions from "../../Pages/Quiz/questions"; // Importiere die Fragen
 import "../../Pages/Quiz/styles.css";
 import film from "../../videos/Particle_Tunnel_4K_Motion_Background_Loop.mp4";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 const Quiz = () => {
   const topics = ["Python", "HTML", "JavaScript"];
@@ -16,7 +18,7 @@ const Quiz = () => {
       const randomIndex = Math.floor(Math.random() * topics.length);
       setSelectedTopic(topics[randomIndex]);
       setIsSpinning(false);
-    }, 2000); // Change the duration as needed
+    }, 2000);
   };
 
   useEffect(() => {
@@ -40,6 +42,8 @@ const Quiz = () => {
     }
   };
 
+  // '<div className="spinner"></div>'
+
   return (
     <div className="container">
       <video autoPlay loop muted className="background-video">
@@ -49,7 +53,11 @@ const Quiz = () => {
       <div className="quiz-container">
         <div className="wheel">
           {isSpinning ? (
-            <div className="spinner"></div>
+            <div className="spinner-container">
+              <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <CircularProgress />
+              </Box>
+            </div>
           ) : (
             <div className="wheel-text">{selectedTopic}</div>
           )}
