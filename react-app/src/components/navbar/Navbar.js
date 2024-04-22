b7636a1af6ed0e06711a201edd2c4d0a3a195ed6
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-04-22 19:00:06 +0200

    .

--- react-app/src/components/navbar/Navbar.js
+++ react-app/src/components/navbar/Navbar.js
@@ -75,23 +75,6 @@ function Navbar() {
               SIGN UP
             </Button>
           )}
-          {/* {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
-          {button && <Button buttonStyle="btn--outline">SIGN IN</Button>} */}
-
-          {/* {button && (
-            <Link to="/sign-in" className="btn-mobile">
-              <button className="btn btn--outline" onClick={closeMobileMenu}>
-                SIGN IN
-              </button>
-            </Link>
-          )}
-          {button && (
-            <Link to="/sign-up" className="btn-mobile">
-              <button className="btn btn--outline" onClick={closeMobileMenu}>
-                SIGN UP
-              </button>
-            </Link>
-          )} */}
         </div>
       </nav>
     </>
