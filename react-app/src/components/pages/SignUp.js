bda304c6884f1651f201bc20aa136996088512c7
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-04-23 10:22:02 +0200

    Korrekturen

--- react-app/src/components/pages/SignUp.js
+++ react-app/src/components/pages/SignUp.js
@@ -1,152 +1,152 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { useAuth } from "../api/auth/AuthProvider"; // Stelle sicher, dass du die useAuth-Hook importierst
-// import "../../App.css";
-// import Avatar from "@mui/material/Avatar";
-// import Button from "@mui/material/Button";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import TextField from "@mui/material/TextField";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import Checkbox from "@mui/material/Checkbox";
-// import Link from "@mui/material/Link";
-// import Grid from "@mui/material/Grid";
-// import Box from "@mui/material/Box";
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-// import Typography from "@mui/material/Typography";
-// import Container from "@mui/material/Container";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../api/auth/AuthProvider"; // Stelle sicher, dass du die useAuth-Hook importierst
+import "../../App.css";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-// const SignUp = () => {
-//   const navigate = useNavigate();
-//   const { signUp } = useAuth(); // Verwende die signUp-Funktion aus der useAuth-Hook
-//   const [formData, setFormData] = useState({
-//     firstName: "",
-//     lastName: "",
-//     email: "",
-//     password: "",
-//     allowExtraEmails: false,
-//   });
+const SignUp = () => {
+  const navigate = useNavigate();
+  const { signUp } = useAuth(); // Verwende die signUp-Funktion aus der useAuth-Hook
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    allowExtraEmails: false,
+  });
 
-//   const handleChange = (e) => {
-//     const { name, value, checked } = e.target;
-//     const val = name === "allowExtraEmails" ? checked : value;
-//     setFormData({ ...formData, [name]: val });
-//   };
+  const handleChange = (e) => {
+    const { name, value, checked } = e.target;
+    const val = name === "allowExtraEmails" ? checked : value;
+    setFormData({ ...formData, [name]: val });
+  };
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       await signUp(formData.email, formData.password); // Benutze die signUp-Funktion, um den Benutzer zu registrieren
-//       navigate("/"); // Leite den Benutzer nach der Registrierung zur Startseite weiter
-//     } catch (error) {
-//       console.error("Registrierung fehlgeschlagen:", error);
-//     }
-//   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(formData.email, formData.password); // Benutze die signUp-Funktion, um den Benutzer zu registrieren
+      navigate("/"); // Leite den Benutzer nach der Registrierung zur Startseite weiter
+    } catch (error) {
+      console.error("Registrierung fehlgeschlagen:", error);
+    }
+  };
 
-//   return (
-//     <ThemeProvider theme={createTheme()}>
-//       <Container component="main" maxWidth="xs">
-//         <CssBaseline />
-//         <Box
-//           sx={{
-//             marginTop: 8,
-//             display: "flex",
-//             flexDirection: "column",
-//             alignItems: "center",
-//           }}
-//         >
-//           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-//             <LockOutlinedIcon />
-//           </Avatar>
-//           <Typography component="h1" variant="h5">
-//             Sign up
-//           </Typography>
-//           <Box
-//             component="form"
-//             noValidate
-//             onSubmit={handleSubmit}
-//             sx={{ mt: 3 }}
-//           >
-//             <Grid container spacing={2}>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                   autoComplete="given-name"
-//                   name="firstName"
-//                   required
-//                   fullWidth
-//                   id="firstName"
-//                   label="Vorname"
-//                   autoFocus
-//                   onChange={handleChange}
-//                 />
-//               </Grid>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="lastName"
-//                   label="Nachname"
-//                   name="lastName"
-//                   autoComplete="family-name"
-//                   onChange={handleChange}
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   id="email"
-//                   label="Email Addresse"
-//                   name="email"
-//                   autoComplete="email"
-//                   onChange={handleChange}
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                   required
-//                   fullWidth
-//                   name="password"
-//                   label="Passwort"
-//                   type="password"
-//                   id="password"
-//                   autoComplete="new-password"
-//                   onChange={handleChange}
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <FormControlLabel
-//                   control={
-//                     <Checkbox
-//                       name="allowExtraEmails"
-//                       color="primary"
-//                       onChange={handleChange}
-//                     />
-//                   }
-//                   label="Ich würde gerne permanent genervt werden mit unnötigen rotz Mails."
-//                 />
-//               </Grid>
-//             </Grid>
-//             <Button
-//               type="submit"
-//               fullWidth
-//               variant="contained"
-//               sx={{ mt: 3, mb: 2 }}
-//             >
-//               Sign Up
-//             </Button>
-//             <Grid container justifyContent="flex-end">
-//               <Grid item>
-//                 <Link href="/sign-in" variant="body2">
-//                   Account bereits vorhanden? Sign-in
-//                 </Link>
-//               </Grid>
-//             </Grid>
-//           </Box>
-//         </Box>
-//       </Container>
-//     </ThemeProvider>
-//   );
-// };
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="Vorname"
+                  autoFocus
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Nachname"
+                  name="lastName"
+                  autoComplete="family-name"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Addresse"
+                  name="email"
+                  autoComplete="email"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Passwort"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name="allowExtraEmails"
+                      color="primary"
+                      onChange={handleChange}
+                    />
+                  }
+                  label="Ich würde gerne permanent genervt werden mit unnötigen rotz Mails."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/sign-in" variant="body2">
+                  Account bereits vorhanden? Sign-in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
-// export default SignUp;
+export default SignUp;
