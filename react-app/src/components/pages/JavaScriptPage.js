b50c7fcf020366f59ffbf593dab94c57275ba6ff
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-04-24 14:00:13 +0200

    add js page & change font

--- react-app/src/components/pages/JavaScriptPage.js
+++ react-app/src/components/pages/JavaScriptPage.js
@@ -8,48 +8,53 @@ function JavaScriptPage() {
         <h1 className="htmlpage-text">JavaScript</h1>
         <div className="contentfield">
           <p className="textfield">
-            <h2>Sturktur</h2>
-            Hypertext Markup Language ist einfach zu editieren und gleicht einer
-            Interpretersprache. HTML-Code kann mit beliebigen Texteditor
-            geschrieben werden und hat keine speziellen Anforderungen bei der
-            Erstellung. Ein Kennzeichen von HTML ist die Verwendung von Tags.
-            Tags sind definierte Elemente, die standardisiert sind. Durch diese
-            Elemente wird eine hierarchische Struktur erreicht. Beim HTML Code
-            sollte darauf geachtet werden, dass alle integrierten Skripte vom
-            Browser geladen werden können. Sobald nämlich ein Fehler auftritt,
-            entsteht ein Bug, welches sich negativ auf die
-            Suchmaschinenoptimierung auswirken kann.
+            <h2>Allgemein</h2>
+            Bei JavaScript handelt es sich um eine Skriptsprache, die auf der
+            Seite des Clients arbeitet. Mit Hilfe von JavaScript lässt sich die
+            Funktionalität von Websites erhöhen, indem Inhalte verändert oder
+            generiert werden. JavaScript wird eingesetzt, um CSS und HTML zu
+            ergänzen.
             <br />
             <br />
-            <h2>HTML-Head</h2>
-            Im Head (oder: HTML-Kopf) werden Meta-Informationen einer Seite
-            festgelegt, die nicht im Browser angezeigt werden. Dort werden
-            wichtige Meta-Tags untergebracht, die für Suchmaschinencrawler
-            relevant sind (z.B. Description-Tag). Damit der Browser die im HTML
-            Head enthaltene Metadaten korrekt lesen und diese mit den richtigen
-            Anwendungen ausführen kann, können viele dieser Daten mit MIME
-            definiert werden.
+            <h2>Einbindung</h2>
+            Der JavaScript Code kann in HTML-Seiten eingebunden werden, damit
+            diese an Funktionalität gewinnen. Hierfür gibt es mehrere
+            Möglichkeiten: er kann zwischen den Tags stehen ("Inline-Variante"),
+            er kann in einer externen Datei untergebracht sein, er kann ein
+            Parameter von HTML-Tags sein und er kann in Form eines HTML-Links
+            eingebaut werden. Bei der Einbindung in den HTML Code ist darauf zu
+            achten, dass das Script keine Fehler aufweist. Somit kann das
+            Entstehen eines Bugs vermieden werden, welches sich negativ auf die
+            Suchmaschinenoptimierung auswirken könnte.
+            <br />
+            Eine Skriptsprache wie JavaScript sollte DOM umsetzen. Das steht für
+            “Document Object Model” und ist eine Empfehlung des W3C. Es handelt
+            sich um ein allgemeines Modell für Objekte eines Dokuments.
             <br />
             <br />
-            <h2>HTML-Body</h2>
-            Im Body (oder: HTML-Körper) befinden sich die eigentlichen Inhalte,
-            die mittels eines Browsers visuell dargestellt werden. Hier werden
-            unter anderem das CSS eines Dokuments, Mediainhalte,
-            Texthervorhebungen und Überschriften hinterlegt.
+            <h2>Anwendung</h2>
+            JavaScript bietet viele verschiedene Anwendungsmöglichkeiten. Es
+            können Seitenbesuche gezählt, Monatskalender eingeblendet oder
+            Formulareingaben überprüft werden. Ein häufig genutztes Beispiel ist
+            auch der Taschenrechner. In Webshops wird der Warenkorb häufig mit
+            JavaScript umgesetzt. Als Erweiterung von JavaScript kann CommonJS
+            dienen. Diese macht unter anderem Programmierschnittstellen möglich,
+            welche die Implementierung in Anwendungen wie Datenbanken,
+            Webservices und Desktop-Anwendungen geeignet sind.
             <br />
             <br />
-            <h2>Syntax</h2>
-            Bei der Verwendung der HTML-Sprache ist es meist essentiell, die
-            richtige Syntax zu verwenden. So werden Tags paarweise deklariert:
-            durch einen Start-Tag z.B. &lt;head&gt; und ein End-Tag z.B.
-            &lt;/head&gt;. Die Definition wird in der Dokumenttypdefinition
-            (DTD) standardmäßig definiert. Durch einen HTML Validator kann diese
-            Syntax automatisch auf Fehler im Dokument überprüft werden.
+            <h2>Sandbox-Prinzip</h2>
+            Das Sandbox-Prinzip ist entwickelt worden, um den Nutzer vor
+            Missbrauch durch JavaScript-Anwendungen zu schützen. Die Sandbox ist
+            dabei eine Art Käfig, die die Sprache in ihrer Reichweite
+            einschränkt. Eine Anwendung kann zum Beispiel nicht auf lokale
+            Dateien zugreifen. Somit können Unbefugte weder Dateien auslesen,
+            noch können sie hinein schreiben.
             <br />
             <br />
             <a
               className="link"
-              href="https://www.w3schools.com/html/default.asp"
+              href="https://www.w3schools.com/js/default.asp"
               target="_blank"
               rel="noreferrer"
             >
