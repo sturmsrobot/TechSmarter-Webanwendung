a07cebee80ee34d3a1b2b36c36345e8bc793d1cd
Tavirp <152269967+Tavirp@users.noreply.github.com>
2024-04-24 03:22:27 +0200

    Create HTML Questions + Answers

---
+++ express-app/__tests__/setup/test-data/QuizHTMLAnswers.js
@@ -0,0 +1,488 @@
+const QuizHTMLAnswers = [
+
+    {
+        quizId: 1,
+        questionId: 1,
+        answerId: 1,
+        answerText: "Hyper Text Markup Language",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 1,
+        answerId: 2,
+        answerText: "Home Tool Markup Language",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 1,
+        answerId: 3,
+        answerText: "Hyperlinks and Text Markup Language",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 2,
+        answerId: 1,
+        answerText: "<ol>",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 2,
+        answerId: 2,
+        answerText: "<ul>",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 2,
+        answerId: 3,
+        answerText: "<li>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 3,
+        answerId: 1,
+        answerText: "Zur Definition einer Division oder eines Abschnitts in einem HTML-Dokument",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 3,
+        answerId: 2,
+        answerText: "Zur Hervorhebung von Text",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 3,
+        answerId: 3,
+        answerText: "Zur Erstellung von Tabellen",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 4,
+        answerId: 1,
+        answerText: "href",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 4,
+        answerId: 2,
+        answerText: "src",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 4,
+        answerId: 3,
+        answerText: "link",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 5,
+        answerId: 1,
+        answerText: "<table>",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 5,
+        answerId: 2,
+        answerText: "<list>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 5,
+        answerId: 3,
+        answerText: "<div>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 6,
+        answerId: 1,
+        answerText: "<Body>",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 6,
+        answerId: 2,
+        answerText: "<Head>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 6,
+        answerId: 3,
+        answerText: "<HTML>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 7,
+        answerId: 1,
+        answerText: "<body>, <head>, <html>",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 7,
+        answerId: 2,
+        answerText: "<!DOCTYPE html>, <html>, <head>, <title>, <body>",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 7,
+        answerId: 3,
+        answerText: "<html>, <head>, <body>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 8,
+        answerId: 1,
+        answerText: "<p>",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 8,
+        answerId: 2,
+        answerText: "<div>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 8,
+        answerId: 3,
+        answerText: "<span>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 9,
+        answerId: 1,
+        answerText: "<a>",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 9,
+        answerId: 2,
+        answerText: "<link>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 9,
+        answerId: 3,
+        answerText: "<href>",
+        isCorrect: true
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 10,
+        answerId: 1,
+        answerText: "<ol>",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 10,
+        answerId: 2,
+        answerText: "<ul>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 10,
+        answerId: 3,
+        answerText: "<list>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 11,
+        answerId: 1,
+        answerText: "<strong>",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 11,
+        answerId: 2,
+        answerText: "<bold>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 11,
+        answerId: 3,
+        answerText: "<b>",
+        isCorrect: true
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 12,
+        answerId: 1,
+        answerText: "<i>",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 12,
+        answerId: 2,
+        answerText: "<em>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 12,
+        answerId: 3,
+        answerText: "<italic>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 13,
+        answerId: 1,
+        answerText: "text",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 13,
+        answerId: 2,
+        answerText: "textarea",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 13,
+        answerId: 3,
+        answerText: "input",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 14,
+        answerId: 1,
+        answerText: "<line>",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 14,
+        answerId: 2,
+        answerText: "<hr>",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 14,
+        answerId: 3,
+        answerText: "<hl>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 15,
+        answerId: 1,
+        answerText: "// Kommentar",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 15,
+        answerId: 2,
+        answerText: "<!-- Kommentar -->",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 15,
+        answerId: 3,
+        answerText: "/* Kommentar */",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 16,
+        answerId: 1,
+        answerText: "<caption>",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 16,
+        answerId: 2,
+        answerText: "<description>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 16,
+        answerId: 3,
+        answerText: "<figcaption>",
+        isCorrect: true
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 17,
+        answerId: 1,
+        answerText: "<form>",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 17,
+        answerId: 2,
+        answerText: "<input>",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 17,
+        answerId: 3,
+        answerText: "<button>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 18,
+        answerId: 1,
+        answerText: "name",
+        isCorrect: true
+    },
+    {
+        quizId: 1,
+        questionId: 18,
+        answerId: 2,
+        answerText: "id",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 18,
+        answerId: 3,
+        answerText: "label",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 19,
+        answerId: 1,
+        answerText: "<dropdown>",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 19,
+        answerId: 2,
+        answerText: "<select>",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 19,
+        answerId: 3,
+        answerText: "<listbox>",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 20,
+        answerId: 1,
+        answerText: "", // "<image src="bild.jpg" alt="Beschreibung">",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 20,
+        answerId: 2,
+        answerText: "", // "<img src="bild.jpg" alt="Beschreibung">",
+        isCorrect: true
+    },    {
+        quizId: 1,
+        questionId: 20,
+        answerId: 3,
+        answerText: "", // "<picture src="bild.jpg" alt="Beschreibung">",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 21,
+        answerId: 1,
+        answerText: "",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 21,
+        answerId: 2,
+        answerText: "",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 21,
+        answerId: 3,
+        answerText: "",
+        isCorrect: false
+    },
+
+
+    {
+        quizId: 1,
+        questionId: 22,
+        answerId: 1,
+        answerText: "",
+        isCorrect: false
+    },
+    {
+        quizId: 1,
+        questionId: 22,
+        answerId: 2,
+        answerText: "",
+        isCorrect: false
+    },    {
+        quizId: 1,
+        questionId: 22,
+        answerId: 3,
+        answerText: "",
+        isCorrect: false
+    },
+    
+]
+module.exports = QuizHTMLAnswers;
