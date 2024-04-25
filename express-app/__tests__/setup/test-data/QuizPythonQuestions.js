9da219f6151e3110583cb70951b94acc1d6db3cb
Tavirp <152269967+Tavirp@users.noreply.github.com>
2024-04-25 16:29:08 +0200

    QuizFragen to merge

---
+++ express-app/__tests__/setup/test-data/QuizPythonQuestions.js
@@ -0,0 +1,104 @@
+const QuizPythonQuestions = [
+    {
+      quizId: 3,
+      questionId: 1,
+      questionText: "Wie kommentiert man in Python Code?",
+    },
+    {
+      quizId: 3,
+      questionId: 2,
+      questionText: "Welche Funktion wird verwendet, um die Länge einer Liste in Python zu erhalten?",
+    },
+    {
+      quizId: 3,
+      questionId: 3,
+      questionText: "Wie definiert man eine Funktion in Python?",
+    },
+    {
+      quizId: 3,
+      questionId: 4,
+      questionText: "Wie importiert man ein Modul in Python?",
+    },
+    {
+      quizId: 3,
+      questionId: 5,
+      questionText: "Welches Schlüsselwort wird verwendet, um eine Bedingung in Python zu definieren?",
+    },
+    {
+      quizId: 3,
+      questionId: 6,
+      questionText: "Wie fügt man ein Element am Ende einer Liste in Python hinzu?",
+    },
+    {
+      quizId: 3,
+      questionId: 7,
+      questionText: "Welches Schlüsselwort wird verwendet, um aus einer Schleife herauszuspringen?",
+    },
+    {
+      quizId: 3,
+      questionId: 8,
+      questionText: "Welche Methode wird verwendet, um den Inhalt einer Liste umzukehren?",
+    },
+    {
+      quizId: 3,
+      questionId: 9,
+      questionText: "Wie erstellt man eine while-Schleife in Python?",
+    },
+    {
+      quizId: 3,
+      questionId: 10,
+      questionText: "Welches Schlüsselwort wird verwendet, um eine Liste zu sortieren?",
+    },
+    {
+      quizId: 3,
+      questionId: 11,
+      questionText: "Welches Schlüsselwort wird verwendet, um auf den letzten Index eines Arrays zuzugreifen?",
+    },
+    {
+      quizId: 3,
+      questionId: 12,
+      questionText: "Welche Funktion wird verwendet, um die kleinste Zahl in einer Liste zu finden?",
+    },
+    {
+      quizId: 3,
+      questionId: 13,
+      questionText: "Wie erstellt man eine leere Liste in Python?",
+    },
+    {
+      quizId: 3,
+      questionId: 14,
+      questionText: "Welches Schlüsselwort wird verwendet, um auf den ersten Index eines Arrays zuzugreifen?",
+    },
+    {
+      quizId: 3,
+      questionId: 15,
+      questionText: "Welche Methode wird verwendet, um die Anzahl der Vorkommen eines Elements in einer Liste zu zählen?",
+    },
+    {
+      quizId: 3,
+      questionId: 16,
+      questionText: "Welches Schlüsselwort wird verwendet, um einen Block von Code zu markieren?",
+    },
+    {
+      quizId: 3,
+      questionId: 17,
+      questionText: "Welche Methode wird verwendet, um eine Liste zu kopieren?",
+    },
+    {
+      quizId: 3,
+      questionId: 18,
+      questionText: "Welche Funktion wird verwendet, um eine Zeichenkette in Großbuchstaben umzuwandeln?",
+    },
+    {
+      quizId: 3,
+      questionId: 19,
+      questionText: "Welches Schlüsselwort wird verwendet, um auf den letzten Wert einer Liste zuzugreifen?",
+    },
+    {
+      quizId: 3,
+      questionId: 20,
+      questionText: "Welche Funktion wird verwendet, um die größte Zahl in einer Liste zu finden?",
+    },
+  ];
+  
+  module.exports = QuizPythonQuestions;
