9262947e8117b012fe02a10e0c5f28198961c710
MauriceTre <140593975+MauriceTre@users.noreply.github.com>
2024-04-25 11:19:20 +0200

    insert code for databse

--- react-app/src/Pages/Highscore/highscore.js
+++ react-app/src/Pages/Highscore/highscore.js
@@ -1,6 +1,27 @@
 import React, { useState } from "react";
 import "./highscore.module.css";
 
+// const HighscoreBoard = () => {
+//   const [highscores, setHighscores] = useState([]);
+
+//   useEffect(() => {
+// Funktion zum Abrufen der Benutzerdaten aus der Datenbank
+// fetchHighscores();
+//   }, []); // Leeres Array als zweites Argument bewirkt, dass der Effekt nur einmal nach dem Rendern ausgeführt wird
+
+//   const fetchHighscores = async () => {
+// try {
+// Hier annehmen, dass die API einen Endpunkt /highscores bereitstellt, der die Highscores zurückgibt
+//   const response = await fetch('/api/highscores'); // Passe den Pfad entsprechend deiner API an
+//   if (!response.ok) {
+// throw new Error('Fehler beim Abrufen der Highscores');
+//   }
+//   const data = await response.json();
+//   setHighscores(data); // Setze die abgerufenen Highscores im Zustand
+// } catch (error) {
+//   console.error('Fehler beim Abrufen der Highscores:', error);
+// }
+//   };
 const HighscoreBoard = () => {
   // Hier könnte die Liste der Highscores aus einer API oder einer anderen Datenquelle geladen werden
   const [highscores, setHighscores] = useState([
