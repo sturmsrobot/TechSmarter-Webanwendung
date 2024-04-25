29d19a37cf6179b61db57a46c512ab2e561de28a
Tavirp <152269967+Tavirp@users.noreply.github.com>
2024-04-26 01:17:24 +0200

    Forgot to commit - commit (Updated routes and models, replaced obscene username in testdata)

--- express-app/__tests__/setup/jest.setup.js
+++ express-app/__tests__/setup/jest.setup.js
@@ -3,28 +3,51 @@ const sequelize = require("../../config/database");
 const TestDataUsers = require("./test-data/TestDataUsers");
 const TestDataStats = require("./test-data/TestDataStats");
 const TestDataQuizzes = require("./test-data/TestDataQuizzes");
-const TestDataAnswer = require("./test-data/TestDataAnswer");
-const TestDataQuestion = require("./test-data/TestDataQuestion");
-const Question = require("../../models/Question");
-const Answer = require("../../models/Answer");
+const TestDataTrophies = require("./test-data/TestDataTrophies");
+const QuizHTMLQuestions = require("./test-data/QuizHTMLQuestions");
+const QuizHTMLAnswers = require("./test-data/QuizHTMLAnswers");
+const QuizJavascriptQuestions = require("./test-data/QuizJavascriptQuestions");
+const QuizJavascriptAnswers = require("./test-data/QuizJavascriptAnswers");
+const QuizPythonQuestions = require("./test-data/QuizPythonQuestions");
+const QuizPythonAnswers = require("./test-data/QuizPythonAnswers");
+
 const User = require("../../models/Users");
 const Stats = require("../../models/Stats");
 const Quizzes = require("../../models/Quizzes");
+const Trophies = require("../../models/Trophy")
+const HTMLQuestions = require("../../models/Questions");
+const HTMLAnswers = require("../../models/Answers");
+const JavascriptQuestions = require("../../models/Questions");
+const JavascriptAnswers = require("../../models/Answers");
+const PythonQuestions = require("../../models/Questions");
+const PythonAnswers = require("../../models/Answers");
 
 module.exports = async () => {
     try {
       await sequelize.dropSchema("users");
       await sequelize.dropSchema("stats");
       await sequelize.dropSchema("quizzes");
-      await sequelize.dropSchema("questions");
-      await sequelize.dropSchema("answers");
+      await sequelize.dropSchema("trophies");
+      await sequelize.dropSchema("htmlquestions");
+      await sequelize.dropSchema("htmlanswers");
+      await sequelize.dropSchema("javascriptquestions");
+      await sequelize.dropSchema("javascriptanswers");
+      await sequelize.dropSchema("pythonquestions");
+      await sequelize.dropSchema("pythonanswers");
+      
       await sequelize.sync();
       // Daten für Tests einfügen
       await User.bulkCreate(TestDataUsers);
       await Stats.bulkCreate(TestDataStats);
       await Quizzes.bulkCreate(TestDataQuizzes);
-      await Question.bulkCreate(TestDataQuestion);
-      await Answer.bulkCreate(TestDataAnswer);
+      await Trophies.bulkCreate(TestDataTrophies);
+      await HTMLQuestions.bulkCreate(QuizHTMLQuestions);
+      await HTMLAnswers.bulkCreate(QuizHTMLAnswers);
+      await JavascriptQuestions.bulkCreate(QuizJavascriptQuestions);
+      await JavascriptAnswers.bulkCreate(QuizJavascriptAnswers);
+      await PythonQuestions.bulkCreate(QuizPythonQuestions)
+      await PythonAnswers.bulkCreate(QuizPythonAnswers);
+
     } catch (e) {
       console.error("MY DB Issue", e);
     }
--- express-app/routes/UserRoutes.js
+++ express-app/routes/UserRoutes.js
@@ -14,41 +14,112 @@ router.get("/", (req, res) => {
       res.status(500).json({ message: "Interner Serverfehler!" });
     });
 });
+router.get("/byId", (req, res) => {
+  const { id } = req.query;
+  User.findOne({ where: { id: id } })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error("Fehler beim Abrufen von Daten aus der Datenbank:", err);
+      res.status(500).json({ message: "Interner Serverfehler!" });
+    });
+});
+router.get("/byUsername", (req, res) => {
+  const { username } = req.query;
+  User.findOne({ where: { username: username } })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error("Fehler beim Abrufen von Daten aus der Datenbank:", err);
+      res.status(500).json({ message: "Interner Serverfehler!" });
+    });
+});
 
 // POST-Anforderungen (Neuen Benutzer erstellen):
 router.post(
   "/",
   [
-    body("id").trim().isEmpty().isNumeric().notEmpty(),
-    body("username").trim().not().isEmpty().isString(),
-    body("email").trim().isEmpty().notEmpty().isEmail(),
-    body("password")
-      .trim()
-      .isEmpty()
-      .notEmpty()
-      .isString()
-      .isLength({ min: 6 }),
+    body("id").trim().isNumeric().notEmpty(),
+    body("username").trim().isString(),
+    body("email").trim().notEmpty().isEmail(),
+    body("password").trim().notEmpty().isString().isLength({ min: 6 }),
+    body("points").trim().isNumeric(),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     // Code, um einen Benutzer zu erstellen
+    const { id, username, email, password, points } = req.body;
+    const User = await User.create({
+      id: id,
+      username: username,
+      email: email,
+      password: password,
+      points: points,
+  });
     res.send("Neuer Benutzer erfolgreich erstellt!");
   }
 );
 
 // PUT-Anforderungen (Benutzerdaten aktualisieren):
-router.put("/:id", (req, res) => {
-  // Code, um Benutzerdaten zu aktualisieren
-  res.send("Benutzerdaten erfolgreich aktualisiert!");
+router.put("/:id",
+[
+  body("id").trim().isNumeric().notEmpty(),
+  body("username").trim().isString(),
+  body("email").trim().notEmpty().isEmail(),
+  body("password").trim().notEmpty().isString().isLength({ min: 6 }),
+  body("points").trim().isNumeric(),
+],
+async (req, res) => {
+  const { id } = req.params;
+  const { username, email, password, points } = req.body;
+
+try {
+  // Finde den User mit der angegebenen ID
+  const user = await User.findOne({ where: { id: id } });
+
+  if (!user) {
+    return res.status(404).json({ message: "User nicht gefunden!" });
+  }
+
+  // Aktualisiere die Userdaten
+  user.username = username;
+  user.email = email;
+  user.password = password;
+  user.points = points;
+  await user.save
+
+  res.json(user);
+} catch (error) {
+  console.error("Fehler beim Aktualisieren des Users:", error);
+  res.status(500).json({ message: "Interner Serverfehler!" });
+}
 });
 
 // DELETE-Anforderungen (Benutzerdaten löschen):
-router.delete("/:id", (req, res) => {
-  // Code, um Benutzerdaten zu löschen
-  res.send("Benutzerdaten erfolgreich gelöscht!");
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Finde den User mit der angegebenen userId
+    const user = await User.findOne({ where: { id: id } });
+
+    if (!User) {
+      return res.status(404).json({ message: "User nicht gefunden!" });
+    }
+
+    // Lösche den User
+    await user.destroy();
+
+    res.json({ message: "User erfolgreich gelöscht!" });
+  } catch (error) {
+    console.error("Fehler beim Löschen des Users:", error);
+    res.status(500).json({ message: "Interner Serverfehler!" });
+  }
 });
 
 // Benutzerpunkte hinzufügen:
