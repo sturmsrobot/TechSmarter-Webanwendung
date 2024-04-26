37741314a6ec0b838f73c2423bde326ac96295d1
Tavirp <152269967+Tavirp@users.noreply.github.com>
2024-04-26 22:10:46 +0200

    Added Javascript + Python Questions

--- express-app/models/Answers.js
+++ express-app/models/Answers.js
@@ -4,34 +4,31 @@ const Question = require("./Questions");
 const { FOREIGNKEYS } = require("sequelize/lib/query-types");
 
 const Answer = sequelize.define(
-  "Answer", 
-{
-  id: {
-  type: DataTypes.INTEGER,
-  autoIncrement: true,
-  primaryKey: true,
-  },
-  quizId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  questionId: {
-    type: DataTypes.INTEGER,
-  },
-  answerId: {
-    type: DataTypes.INTEGER,
-  },
-  answerText: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  isCorrect: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-});
-
-Answer.belongsTo(Question);
-
-module.exports = Answer;
+    "Answer", 
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    questionId: {
+      type: DataTypes.INTEGER,
+    },
+    answerId: {
+      type: DataTypes.INTEGER,
+    },
+    answerText: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isCorrect: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  });
+  
+  Answer.belongsTo(Question);
+  
+  module.exports = Answer;
+  
